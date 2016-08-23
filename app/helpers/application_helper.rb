4d12b78a9fdd5cc1af09642dde6dcbcff45739bf
Rob Nichols <robnichols@warwickshire.gov.uk>
2016-08-23 14:54:46 +0100

    Add Bootstrap styling and sign in links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def nav_link_to(label, path, link_args={})
+    list_args = {}
+    if current_page?(path)
+      list_args[:class] = 'active'
+      path = '#'
+    end
+    link = link_to(label, path, link_args)
+    content_tag 'li', link, list_args
+  end
+
 end
